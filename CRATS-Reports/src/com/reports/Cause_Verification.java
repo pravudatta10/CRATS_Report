a9ce271d0b7c745d0f594ab7648157308b18f273
deepakgullapalli <deepakgullapalli9701@gmail.com>
2024-02-08 15:17:43 +0530

    commit

--- CRATS-Reports/src/com/reports/Cause_Verification.java
+++ CRATS-Reports/src/com/reports/Cause_Verification.java
@@ -13,7 +13,7 @@ import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
-public class Cause_Verification {
+public class Cause_Verification { 
 	static Font boldFontHeading = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
 	static Font normalBold = new Font(Font.FontFamily.HELVETICA, 9, Font.BOLD); 
 	static Font normalFont = new Font(Font.FontFamily.HELVETICA, 9);
